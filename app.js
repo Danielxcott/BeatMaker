77f230a39e69b0a4fc1f5d0aaeef871f54e49133
Danielxcott <changwilliam826@gmail.com>
2022-06-29 18:25:19 +0630

    add more songs and choose more options

--- app.js
+++ app.js
@@ -1,98 +1,131 @@
 const kickSelect = document.querySelector("#kickSelect");
 const snareSelect = document.querySelector("#snareSelect");
-const hihatSelect = document.querySelector("#hihatSelect")
-
+const hihatSelect = document.querySelector("#hihatSelect");
 
 //Option Start
-kickSongs.forEach((songs,index)=>{
-    kickSelect.insertAdjacentHTML("beforeend",`<option value="${songs.path}">${songs.name}</option>`)
-})
+kickSongs.forEach((songs, index) => {
+  kickSelect.insertAdjacentHTML(
+    "beforeend",
+    `<option value="${songs.path}">${songs.name}</option>`
+  );
+});
 
-snareSongs.forEach((songs,index)=>{
-    snareSelect.insertAdjacentHTML("beforeend",`<option value="${songs.path}">${songs.name}</option>`)
-})
+snareSongs.forEach((songs, index) => {
+  snareSelect.insertAdjacentHTML(
+    "beforeend",
+    `<option value="${songs.path}">${songs.name}</option>`
+  );
+});
 
-hihatSongs.forEach((songs,index)=>{
-    hihatSelect.insertAdjacentHTML("beforeend",`<option value="${songs.path}">${songs.name}</option>`)
-})
+hihatSongs.forEach((songs, index) => {
+  hihatSelect.insertAdjacentHTML(
+    "beforeend",
+    `<option value="${songs.path}">${songs.name}</option>`
+  );
+});
 //Option End
 
-class DrumKit{
-    constructor(){
-        this.pads = document.querySelectorAll(".pad");
-        this.kickAudio = document.querySelector(".kick-sound");
-        this.snareAudio = document.querySelector(".snare-sound");
-        this.hihatAudio = document.querySelector(".hihat-sound");
-        this.play = document.querySelector(".play");
-        this.index = 0;
-        this.bpm = 120;
-        this.isPlaying = null;
-    }
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.play = document.querySelector(".play");
+    this.selects = document.querySelectorAll("select");
+    this.index = 0;
+    this.bpm = 120;
+    this.isPlaying = null;
+  }
 
-    activePads(){
-        this.classList.toggle("active");
-    }
+  activePads() {
+    this.classList.toggle("active");
+  }
 
-    repeat(){
-        let step = this.index % 8;
-        const activeBars = document.querySelectorAll(`.b${step}`);
-        activeBars.forEach(bar=>{
-            bar.style.animation=`playTrack 0.5s alternate ease-in-out 2`;
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.5s alternate ease-in-out 2`;
 
-            //adding sound if pads are active
-            if(bar.classList.contains("active")){
-                if(bar.classList.contains("kick-pad")){
-                    this.kickAudio.currentTime = 0;
-                    this.kickAudio.play();
-                }
-                if(bar.classList.contains("snare-pad")){
-                    this.snareAudio.currentTime = 0;
-                    this.snareAudio.play();
-                }
-                if(bar.classList.contains("hihat-pad")){
-                    this.hihatAudio.currentTime = 0;
-                    this.hihatAudio.play();
-                }
-            }
-        })
-        
-        this.index++;
-    }
-    start(){
-        const interval = (60 / this.bpm) * 1000;
-        //fix play button 
-        if(!this.isPlaying){
-          this.isPlaying = setInterval(()=>{
-                this.repeat();
-            },interval)
-        }else{
-            clearInterval(this.isPlaying);
-            this.isPlaying = null;
-        }  
-    }
-    //change the text button 
-    updateBtn(){
-        if(!this.isPlaying){
-            this.play.innerText = "Play";
-            this.play.classList.remove("active");
-        }else{
-            this.play.innerText = "Stop";
-            this.play.classList.add("active");
+      //adding sound if pads are active
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
         }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
+
+    this.index++;
+  }
+  
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    //fix play button
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    } else {
+      clearInterval(this.isPlaying);
+      this.isPlaying = null;
     }
+  }
+
+  //change the text button
+  updateBtn() {
+    if (!this.isPlaying) {
+      this.play.innerText = "Play";
+      this.play.classList.remove("active");
+    } else {
+      this.play.innerText = "Stop";
+      this.play.classList.add("active");
+    }
+  }
+
+  //update the select options depend on user's choose
+  updateSong(e) {
+    let selectName = e.target.name;
+    const selectValue = e.target.value;
+    switch (selectName) {
+      case "kick-select":
+        this.kickAudio.src = selectValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = selectValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = selectValue;
+        break;
+    }
+  }
 }
 
 const drumkit = new DrumKit();
 
-drumkit.pads.forEach((pad)=>{
-    pad.addEventListener("click",drumkit.activePads);
-    pad.addEventListener("animationend",function(){
-        this.style.animation = "";
-    })
-})
+drumkit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumkit.activePads);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
 
-drumkit.play.addEventListener("click",()=>{
-    drumkit.start();
-    drumkit.updateBtn();
+drumkit.play.addEventListener("click", () => {
+  drumkit.start();
+  drumkit.updateBtn();
+});
 
-})
+drumkit.selects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    drumkit.updateSong(e);
+  });
+});
--- data.js
+++ data.js
@@ -1,41 +1,244 @@
-const kickSongs =[
-    {
-        name:"Kick-808",
-        path:"./allSounds/kick-808.wav"
-    },
-    {
-        name:"Kick-acoustic",
-        path:"./allSounds/kick-acoustic01.wav"
-    },{
-        name:"Kick-acoustic2",
-        path:"./allSounds/kick-acoustic02.wav"
-    }
-]
+const kickSongs = [
+  {
+    name: "Kick-808",
+    path: "./allSounds/kick-808.wav",
+  },
+  {
+    name: "Kick-acoustic",
+    path: "./allSounds/kick-acoustic01.wav",
+  },
+  {
+    name: "Kick-acoustic2",
+    path: "./allSounds/kick-acoustic02.wav",
+  },
+  {
+    name: "Kick-big",
+    path: "./allSounds/kick-big.wav",
+  },
+  {
+    name: "Kick-classic",
+    path: "./allSounds/kick-classic.wav",
+  },
+  {
+    name: "Kick-cultivator",
+    path: "./allSounds/kick-cultivator.wav",
+  },
+  {
+    name: "Kick-deep",
+    path: "./allSounds/kick-deep.wav",
+  },
+  {
+    name: "Kick-dry",
+    path: "./allSounds/kick-dry.wav",
+  },
+  {
+    name: "Kick-electro01",
+    path: "./allSounds/kick-electro01.wav",
+  },
+  {
+    name: "Kick-electro02",
+    path: "./allSounds/kick-electro02.wav",
+  },
+  {
+    name: "Kick-floppy",
+    path: "./allSounds/kick-floppy.wav",
+  },
+  {
+    name: "Kick-gritty",
+    path: "./allSounds/kick-gritty.wav",
+  },
+  {
+    name: "Kick-heavy",
+    path: "./allSounds/kick-heavy.wav",
+  },
+  {
+    name: "Kick-newwave",
+    path: "./allSounds/kick-newwave.wav",
+  },
+  {
+    name: "Kick-oldschool",
+    path: "./allSounds/kick-oldschool.wav",
+  },
+  {
+    name: "Kick-plain",
+    path: "./allSounds/kick-plain.wav",
+  },
+  {
+    name: "Kick-slapback",
+    path: "./allSounds/kick-slapback.wav",
+  },
+  {
+    name: "Kick-softy",
+    path: "./allSounds/kick-softy.wav",
+  },
+  {
+    name: "Kick-stomp",
+    path: "./allSounds/kick-stomp.wav",
+  },
+  {
+    name: "Kick-tape",
+    path: "./allSounds/kick-tape.wav",
+  },
+  {
+    name: "Kick-thump",
+    path: "./allSounds/kick-thump.wav",
+  },
+  {
+    name: "Kick-tight",
+    path: "./allSounds/kick-tight.wav",
+  },
+  {
+    name: "Kick-tron",
+    path: "./allSounds/kick-tron.wav",
+  },
+  {
+    name: "Kick-vinyl01",
+    path: "./allSounds/kick-vinyl01.wav",
+  },
+  {
+    name: "Kick-vinyl02",
+    path: "./allSounds/kick-vinyl02.wav",
+  },
+  {
+    name: "Kick-zapper",
+    path: "./allSounds/kick-zapper.wav",
+  },
+];
 
-const snareSongs =[
-    {
-        name:"Snare-808",
-        path:"./allSounds/snare-808.wav"
-    },
-    {
-        name:"Snare-acoustic",
-        path:"./allSounds/snare-acoustic01.wav"
-    },{
-        name:"Snare-acoustic2",
-        path:"./allSounds/snare-acoustic02.wav"
-    }
-]
+const snareSongs = [
+  {
+    name: "Snare-808",
+    path: "./allSounds/snare-808.wav",
+  },
+  {
+    name: "Snare-acoustic",
+    path: "./allSounds/snare-acoustic01.wav",
+  },
+  {
+    name: "Snare-acoustic2",
+    path: "./allSounds/snare-acoustic02.wav",
+  },
+  {
+    name: "Snare-analog",
+    path: "./allSounds/snare-analog.wav",
+  },
+  {
+    name: "Snare-big",
+    path: "./allSounds/snare-big.wav",
+  },
+  {
+    name: "Snare-block",
+    path: "./allSounds/snare-block.wav",
+  },
+  {
+    name: "Snare-brute",
+    path: "./allSounds/snare-brute.wav",
+  },
+  {
+    name: "Snare-dist01",
+    path: "./allSounds/snare-dist01.wav",
+  },
+  {
+    name: "Snare-dist02",
+    path: "./allSounds/snare-dist02.wav",
+  },
+  {
+    name: "Snare-dist03",
+    path: "./allSounds/snare-dist03.wav",
+  },
+  {
+    name: "Snare-electro",
+    path: "./allSounds/snare-electro.wav",
+  },
+  {
+    name: "Snare-lofi01",
+    path: "./allSounds/snare-lofi01.wav",
+  },
+  {
+    name: "Snare-lofi02",
+    path: "./allSounds/snare-lofi02.wav",
+  },
+  {
+    name: "Snare-modular",
+    path: "./allSounds/snare-modular.wav",
+  },
+  {
+    name: "Snare-noise",
+    path: "./allSounds/snare-noise.wav",
+  },
+  {
+    name: "Snare-pinch",
+    path: "./allSounds/snare-pinch.wav",
+  },
+  {
+    name: "Snare-punch",
+    path: "./allSounds/snare-punch.wav",
+  },
+  {
+    name: "Snare-smasher",
+    path: "./allSounds/snare-smasher.wav",
+  },
+  {
+    name: "Snare-sumo",
+    path: "./allSounds/snare-sumo.wav",
+  },
+  {
+    name: "Snare-tape",
+    path: "./allSounds/snare-tape.wav",
+  },
+  {
+    name: "Snare-vinyl01",
+    path: "./allSounds/snare-vinyl01.wav",
+  },
+  {
+    name: "Snare-vinyl02",
+    path: "./allSounds/snare-vinyl02.wav",
+  },
+];
 
-const hihatSongs =[
-    {
-        name:"Hihat-808",
-        path:"./allSounds/hihat-808.wav"
-    },
-    {
-        name:"Hihat-acoustic",
-        path:"./allSounds/hihat-acoustic01.wav"
-    },{
-        name:"Hihat-acoustic2",
-        path:"./allSounds/hihat-acoustic02.wav"
-    }
-]
+const hihatSongs = [
+  {
+    name: "Hihat-808",
+    path: "./allSounds/hihat-808.wav",
+  },
+  {
+    name: "Hihat-acoustic",
+    path: "./allSounds/hihat-acoustic01.wav",
+  },
+  {
+    name: "Hihat-acoustic2",
+    path: "./allSounds/hihat-acoustic02.wav",
+  },
+  {
+    name: "Hihat-analog",
+    path: "./allSounds/hihat-analog.wav",
+  },
+  {
+    name: "Hihat-digital",
+    path: "./allSounds/hihat-digital.wav",
+  },
+  {
+    name: "Hihat-dist01",
+    path: "./allSounds/hihat-dist01.wav",
+  },
+  {
+    name: "Hihat-dist02",
+    path: "./allSounds/hihat-dist02.wav",
+  },
+  {
+    name: "Hihat-electro",
+    path: "./allSounds/hihat-electro.wav",
+  },
+  {
+    name: "Hihat-plain",
+    path: "./allSounds/hihat-plain.wav",
+  },
+  {
+    name: "Hihat-reso",
+    path: "./allSounds/hihat-reso.wav",
+  },
+  {
+    name: "Hihat-ring",
+    path: "./allSounds/hihat-ring.wav",
+  },
+];
